3d762ddd5dec9e6d3670c3557b9f62cf1e02df43
hardikp-ezdi <hardik.p@ezdi.us>
2016-09-29 19:54:11 +0530

    First commit

---
+++ src/main/java/net/codejava/hibernate/Category.java
@@ -0,0 +1,59 @@
+package net.codejava.hibernate;
+ 
+import java.util.Set;
+ 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+ 
+ 
+@Entity
+@Table(name = "CATEGORY")
+public class Category {
+ 
+    public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public void setProducts(Set<Product> products) {
+		this.products = products;
+	}
+
+	private long id;
+    private String name;
+ 
+    private Set<Product> products;
+ 
+    public Category() {
+    }
+ 
+    public Category(String name) {
+        this.name = name;
+    }
+ 
+    @Id
+    @Column(name = "CATEGORY_ID")
+    @GeneratedValue
+    public long getId() {
+        return id;
+    }
+ 
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL)
+    public Set<Product> getProducts() {
+        return products;
+    }
+ 
+    // other getters and setters...
+}
