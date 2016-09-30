b7c0088f6820d580821471a89d6709f6a9da0b5b
hardikp-ezdi <hardik.p@ezdi.us>
2016-09-30 18:34:49 +0530

    commit

---
+++ src/main/java/net/codejava/hibernate/Test.java
@@ -0,0 +1,15 @@
+package net.codejava.hibernate;
+
+public class Test {
+	
+public static void main(String args[])
+{
+	System.out.println("Hi");
+
+	System.out.println("hardik patel");
+	
+
+	System.out.println("Hi");
+
+}
+}
