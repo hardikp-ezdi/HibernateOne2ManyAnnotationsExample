b7b64d30895eaa8f785ab46d9c302bed02dcc246
hardikp-ezdi <hardik.p@ezdi.us>
2016-09-30 19:14:07 +0530

    commit

--- src/main/java/net/codejava/hibernate/Test.java
+++ src/main/java/net/codejava/hibernate/Test.java
@@ -10,7 +10,8 @@ public static void main(String args[])
 	System.out.println("Hi");
 	System.out.println("Hi");
 	System.out.println("Hi");
-	System.out.println("Hi");
+
+	System.out.println("update in local");
 
 }
 }
